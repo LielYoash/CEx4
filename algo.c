9ab628c7838c5bc097afd430657b9b150edbfb1c
LielYoash <lielyoash@LielYoash.myguest.virtualbox.org>
2023-01-15 21:45:00 +0200

    added differnt functions to algo

--- algo.c
+++ algo.c
@@ -6,30 +6,88 @@ void A()
     int numOfNodes, startNodeId, endNodeId, weight;
     char init;
     scanf("%d", &numOfNodes);
+    if (exist(0) == 1)
+    {
+        deleteGraph;
+    }
     for (int i = 0; i < numOfNodes; i++)
     {
         createNode(i);
     }
     if (scanf("%c", &init) == 'n')
     {
-    nodesSetUp:
-        scanf("%d %d %d", &startNode, &endNode, &weight);
-        addEdge(startNode, endNode, weight);
-        while (scanf("%c", &init) != 'n')
-        {
-            scanf("%d %d", &endNode, &weight);
-            addEdge(startNode, endNode, weight);
-        }
-        if (init == 'n')
-        {
-            goto nodesSetUp;
-        }
-        else
+        addEdges();
+    }
+}
+
+void addEdges()
+{
+    int startNodeId, endNodeId, weight;
+    char c;
+    scanf("%d %d %d", &startNodeId, &endNodeId, &weight);
+    createEdge(startNodeId, endNodeId, weight);
+    while (scanf("%c", &c) != 'n')
+    {
+        if (isComplete() == 1)
         {
             return;
         }
+        else
+        {
+            scanf("%d %d", &endNodeId, &weight);
+            createEdge(startNodeId, endNodeId, weight);
+        }
+        if (scanf("%c", &c) == 'n')
+        {
+            addEdges();
+        }
     }
 }
+
+void deleteGraph()
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        edge *temp2 = temp->edge;
+        while (temp2 != NULL)
+        {
+            edge *temp3 = temp2;
+            temp2 = temp2->next;
+            free(temp3);
+        }
+        node *temp4 = temp;
+        temp = temp->next;
+        free(temp4);
+    }
+}
+
+int isComplete()
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        edge *temp2 = temp->edge;
+        while (temp2 != NULL)
+        {
+            if (temp2->endNode == temp->nodeID)
+            {
+                temp2 = temp2->next;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        temp = temp->next;
+    }
+    return 1
+}
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void B()
 {
