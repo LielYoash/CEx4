c51e6088be89fb172df5fcf9444a26f244e3a039
LielYoash <lielyoash@LielYoash.myguest.virtualbox.org>
2023-01-19 13:14:08 +0200

    fixed S and T

--- algo.c
+++ algo.c
@@ -4,8 +4,9 @@
 #include "edges.h"
 #include "algo.h"
 #include <ctype.h>
+#include <stdbool.h>
 
-#define MAX 1000000
+#define INT_MAX 1000000
 
 void funcHelper(int startNodeID, int endNodeId, int weight, node *head);
 void deleteGraph(node *head);
@@ -47,14 +48,14 @@ void deleteGraph(node *head)
     }
 }
 
-int min(int a, int b){
+int max(int a, int b){
     if(a==0){
         return b;
     }
     if(b==0){
         return a;
     }
-    if(a<b){
+    if(a>b){
         return a;
     }
     else{
@@ -67,6 +68,7 @@ int min(int a, int b){
 
 void A(node *head)
 {
+    deleteGraph(head);
     int numOfNodes, startNodeId, endNodeId, weight;
     char init;
     scanf("%d", &numOfNodes);
@@ -140,109 +142,168 @@ void S(node *head)
     printf("%d", shortestPathFunc(startNodeID, endNodeID, head));
 }
 
-int shortestPathFunc( int source, int target, node *head)
-{
-    int *dist = malloc(sizeof(int) * 100);
-    int *prev = malloc(sizeof(int) * 100);
-    int *visited = malloc(sizeof(int) * 100);
-    for (int i = 0; i < 100; i++)
-    {
-        dist[i] = 1000000;
-        prev[i] = -1;
-        visited[i] = 0;
+int shortestPathFunc(int startNodeID, int endNodeID, node *head) {
+    if (!head) return -1;
+    int n = 0;
+    node *curr = head;
+    while (curr) {
+        n = max(n, curr->nodeID);
+        curr = curr->next;
     }
-    dist[source] = 0;
-    node *temp = head;
-    while (temp != NULL)
-    {
-        int min = 1000000;
-        int minIndex = -1;
-        for (int i = 0; i < 100; i++)
-        {
-            if (visited[i] == 0 && dist[i] < min)
-            {
-                min = dist[i];
-                minIndex = i;
+    n++;
+    int dist[n];
+    bool visited[n];
+    int heap[n], pos[n];
+    int size = 0;
+
+    for (int i = 0; i < n; i++) {
+        dist[i] = INT_MAX;
+        visited[i] = false;
+    }
+
+    dist[startNodeID] = 0;
+    heap[size] = startNodeID;
+    pos[startNodeID] = size;
+    size++;
+
+    while (size != 0) {
+        int u = heap[0], v;
+        pos[u] = pos[heap[size - 1]];
+        heap[0] = heap[size - 1];
+        size--;
+        int index = 0;
+        do {
+            index = (2 * index + 1);
+            if ((2 * index + 2 < size) && dist[heap[2 * index + 2]] < dist[heap[index]])
+                index = 2 * index + 2;
+            if (index < size) {
+                int temp = heap[index];
+                heap[index] = heap[pos[u]];
+                heap[pos[u]] = temp;
+                pos[heap[index]] = index;
+                pos[u] = pos[u];
+            }
+        } while (index < size);
+        visited[u] = true;
+        node *curr = head;
+        while (curr && curr->nodeID != u) {
+            curr = curr->next;
+        }
+        if (!curr) continue;
+        for (edge *e = curr->edge; e; e = e->next) {
+            v = e->endNode;
+            int weight = e->weight;
+            if (!visited[v] && dist[u] + weight < dist[v]) {
+                dist[v] = dist[u] + weight;
+                if (pos[v] == -1) {
+                    heap[size] = v;
+                    pos[v] = size;
+                    size++;
+                }
+                int temp = pos[v];
+                while (temp > 0 && dist[heap[(temp - 1) / 2]] > dist[heap[temp]]) {
+                    int t = heap[temp];
+                    heap[temp] = heap[(temp - 1) / 2];
+                    heap[(temp - 1) / 2] = t;
+                    pos[heap[temp]] = temp;
+                    pos[heap[(temp - 1) / 2]] = (temp - 1) / 2;
+                    temp = (temp - 1) / 2;
+                }
             }
         }
-        if (minIndex == -1)
-        {
-            break;
-        }
-        visited[minIndex] = 1;
-        edge *temp2 = temp->edge;
-        while (temp2)
-        {
-            if (dist[temp2->endNode] > dist[minIndex] + temp2->weight)
-            {
-                dist[temp2->endNode] = dist[minIndex] + temp2->weight;
-                prev[temp2->endNode] = minIndex;
-            }
-            temp2 = temp2->next;
-        }
-        temp = temp->next;
     }
-    int path = 0;
-    int temp3 = target;
-    while (temp3 != source)
-    {
-        path += dist[temp3];
-        temp3 = prev[temp3];
-    }
-    return path;
+    if (dist[endNodeID] == INT_MAX) return -1;
+    return dist[endNodeID];
 }
+
   
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void T(node *head){
-    int *dist = malloc(sizeof(int) * 100);
-    int *prev = malloc(sizeof(int) * 100);
-    int *visited = malloc(sizeof(int) * 100);
-    for (int i = 0; i < 100; i++)
+void T(node *head)
+{
+    int count;
+    scanf("%d", &count);
+    if (count == 0)
     {
-        dist[i] = 1000000;
-        prev[i] = -1;
-        visited[i] = 0;
+        return;
     }
-    dist[0] = 0;
-    node *temp = head;
-    while (temp != NULL)
+    int cities[count];
+    for (size_t i = 0; i < count; i++)
     {
-        int min = 1000000;
-        int minIndex = -1;
-        for (int i = 0; i < 100; i++)
-        {
-            if (visited[i] == 0 && dist[i] < min)
-            {
-                min = dist[i];
-                minIndex = i;
-            }
-        }
-        if (minIndex == -1)
-        {
-            break;
-        }
-        visited[minIndex] = 1;
-        edge *temp2 = temp->edge;
-        while (temp2)
-        {
-            if (dist[temp2->endNode] > dist[minIndex] + temp2->weight)
-            {
-                dist[temp2->endNode] = dist[minIndex] + temp2->weight;
-                prev[temp2->endNode] = minIndex;
-            }
-            temp2 = temp2->next;
-        }
-        temp = temp->next;
+        scanf("%d", &cities[i]);
     }
-    int path = 0;
-    int temp3 = 0;
-    while (temp3 != 0)
+    int fac = factorial(count);
+    int perm[fac];
+    int temp = 0;
+    permutation(head, cities, 0, count - 1, perm, &temp);
+    int ind = find_minimum(perm, fac);
+    if (perm[ind] == INT_MAX)
     {
-        path += dist[temp3];
-        temp3 = prev[temp3];
+        printf("TSP shortest path: %d \n", -1);
     }
-    printf("%d", path);
+    else
+    {
+        printf("TSP shortest path: %d \n", perm[ind]);
+    }
+}
+
+int arrayCalculate(node *head, int cities[], int size)
+{
+    int distance = 0;
+    for (size_t i = 0; i < size - 1; i++)
+    {
+        int path = shortestPathFunc(head, cities[i], cities[i + 1]);
+        if (path == -1)
+        {
+            return INT_MAX;
+        }
+        distance += path;
+    }
+    return distance;
+}
+
+void swap(int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void permutation(node *head, int *cities, int start, int end, int *perm, int *ind)
+{
+    if (start == end)
+    {
+        perm[(*ind)++] = arrayCalculate(head, cities, end + 1);
+        return;
+    }
+    int i;
+    for (i = start; i <= end; i++)
+    {
+        swap((cities + i), (cities + start));
+        permutation(head, cities, start + 1, end, perm, ind);
+        swap((cities + i), (cities + start));
+    }
+}
+
+int find_minimum(int arr[], int n)
+{
+    int index = 0;
+    for (int i = 1; i < n; i++)
+        if (arr[i] < arr[index])
+        {
+            index = i;
+        }
+    return index;
+}
+
+int factorial(int num)
+{
+    if (num == 0)
+        return 1;
+    if (num < 0)
+        return 0;
+    return factorial(num - 1) * num;
 }
